88e3a7e96fd8520ba92928b02eaa8b756c4e8cf3
jh0x <j@hulboj.org>
2019-09-08 15:11:55 +0100

    Toolbox: Utoa - fixed even utoa

--- include/toolbox/NumberStringUtils.h
+++ include/toolbox/NumberStringUtils.h
@@ -2,7 +2,7 @@
 
 #include <toolbox/Config.h>
 
-#include <cstdint>
+#include <cstddef>
 #include <type_traits>
 
 TOOLBOX_NAMESPACE_BEGIN
@@ -25,6 +25,45 @@ inline constexpr int countDigits(T n, CountWithUnroll tag = {})
 		result += 4;
 	}
 	return result;
+}
+
+
+// Based on: https://github.com/miloyip/itoa-benchmark
+
+inline constexpr char _kHundredNumbers[200] = {
+'0','0','1','0','2','0','3','0','4','0','5','0','6','0','7','0','8','0','9','0',
+'0','1','1','1','2','1','3','1','4','1','5','1','6','1','7','1','8','1','9','1',
+'0','2','1','2','2','2','3','2','4','2','5','2','6','2','7','2','8','2','9','2',
+'0','3','1','3','2','3','3','3','4','3','5','3','6','3','7','3','8','3','9','3',
+'0','4','1','4','2','4','3','4','4','4','5','4','6','4','7','4','8','4','9','4',
+'0','5','1','5','2','5','3','5','4','5','5','5','6','5','7','5','8','5','9','5',
+'0','6','1','6','2','6','3','6','4','6','5','6','6','6','7','6','8','6','9','6',
+'0','7','1','7','2','7','3','7','4','7','5','7','6','7','7','7','8','7','9','7',
+'0','8','1','8','2','8','3','8','4','8','5','8','6','8','7','8','8','8','9','8',
+'0','9','1','9','2','9','3','9','4','9','5','9','6','9','7','9','8','9','9','9',
+};
+
+template<size_t N, typename T>
+inline constexpr bool utoaFixedEven(char * buffer, T t)
+{
+	static_assert(std::is_unsigned_v<T>, "Only unsigned");
+	static_assert(N % 2 == 0, "Only even length");
+	buffer += N;
+	size_t count = N;
+	while(count > 2)
+	{
+		const auto i = (t % 100) << 1;
+		t /= 100;
+		*--buffer = _kHundredNumbers[i];
+		*--buffer = _kHundredNumbers[i + 1];
+		count -= 2;
+	}
+	{
+		const auto i = t << 1;
+		*--buffer = _kHundredNumbers[i];
+		*--buffer = _kHundredNumbers[i + 1];
+	}
+	return !(t / 100);
 }
 
 TOOLBOX_NAMESPACE_END
--- test/NumberStringUtils.cpp
+++ test/NumberStringUtils.cpp
@@ -2,6 +2,9 @@
 
 #include <cstdint>
 #include <limits>
+#include <string>
+#include <string_view>
+#include <type_traits>
 
 #include <toolbox/NumberStringUtils.h>
 
@@ -46,4 +49,62 @@ TEST_CASE("NumberStringUtils - countDigits") {
 	REQUIRE(t::countDigits(12345678901234567890ull) == 20);
 	REQUIRE(t::countDigits(std::numeric_limits<uint64_t>::max()) == 20);
 	REQUIRE(t::countDigits(std::numeric_limits<__uint128_t>::max()) == 39);
+}
+
+TEST_CASE("NumberStringUtils - utoaFixedEven") {
+	auto test = [](
+			auto ntype
+			, auto x
+			, const std::string& expected
+			, auto shouldWork) {
+		constexpr size_t n = ntype();
+		INFO("Width = " << n
+				<< ", x = " << x
+				<< ", expected = " << expected
+				<< ", shouldWork = " << shouldWork);
+		char buf[n];
+		const auto res = t::utoaFixedEven<n>(buf, x);
+		REQUIRE(res == shouldWork);
+		if(shouldWork) {
+			REQUIRE(std::string_view(buf, n) == expected);
+		}
+	};
+	test(std::integral_constant<size_t, 2>{}, 0u, "00", true);
+	test(std::integral_constant<size_t, 2>{}, 5u, "05", true);
+	test(std::integral_constant<size_t, 2>{}, 52u, "52", true);
+	test(std::integral_constant<size_t, 2>{}, 100u, "", false);
+
+	test(std::integral_constant<size_t, 4>{}, 0u, "0000", true);
+	test(std::integral_constant<size_t, 4>{}, 1u, "0001", true);
+	test(std::integral_constant<size_t, 4>{}, 12u, "0012", true);
+	test(std::integral_constant<size_t, 4>{}, 123u, "0123", true);
+	test(std::integral_constant<size_t, 4>{}, 1234u, "1234", true);
+	test(std::integral_constant<size_t, 4>{}, 10000u, "", false);
+
+	test(std::integral_constant<size_t, 6>{}, 0u, "000000", true);
+	test(std::integral_constant<size_t, 6>{}, 9u, "000009", true);
+	test(std::integral_constant<size_t, 6>{}, 98u, "000098", true);
+	test(std::integral_constant<size_t, 6>{}, 987u, "000987", true);
+	test(std::integral_constant<size_t, 6>{}, 9876u, "009876", true);
+	test(std::integral_constant<size_t, 6>{}, 987654u, "987654", true);
+	test(std::integral_constant<size_t, 6>{}, 9876543u, "9876543", false);
+
+	test(std::integral_constant<size_t, 10>{}, 0u, "0000000000", true);
+	test(std::integral_constant<size_t, 10>{}, 1u, "0000000001", true);
+	test(std::integral_constant<size_t, 10>{}, 12u, "0000000012", true);
+	test(std::integral_constant<size_t, 10>{}, 123u, "0000000123", true);
+	test(std::integral_constant<size_t, 10>{}, 1234u, "0000001234", true);
+	test(std::integral_constant<size_t, 10>{}, 12345u, "0000012345", true);
+	test(std::integral_constant<size_t, 10>{}, 123456u, "0000123456", true);
+	test(std::integral_constant<size_t, 10>{}, 1234567u, "0001234567", true);
+	test(std::integral_constant<size_t, 10>{}, 12345678u, "0012345678", true);
+	test(std::integral_constant<size_t, 10>{}, 123456789u, "0123456789", true);
+	test(std::integral_constant<size_t, 10>{}, 1234567890u, "1234567890", true);
+
+	test(std::integral_constant<size_t, 20>{}, 12345678900987654321ull
+			, "12345678900987654321", true);
+
+	test(std::integral_constant<size_t, 40>{}
+			, std::numeric_limits<__uint128_t>::max()
+			, "0340282366920938463463374607431768211455", true);
 }
